980b9dfce6ea952c6e16dd8fe936dd160bb60d90
cuc dat <dat.huynhcuc@hcmut.edu.vn>
2026-05-09 20:53:23 +0700

    fix: re-track src/models/

---
+++ src/models/mlp.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+
+class MLPClassifier(nn.Module):
+    def __init__(self, input_dim: int = 10, num_classes: int = 3):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 32),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+
+            nn.Linear(16, num_classes)
+        )
+
+    def forward(self, x):
+        return self.net(x)
